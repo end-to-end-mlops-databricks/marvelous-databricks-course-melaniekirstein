54aefc764247b3fe628985b8b073f00802bcbf6e
GERKEM01 <GERKEM01@HEIWAY.NET>
2024-10-31 10:16:40 +0200

    refactor(dataset): Refactor 01.prepare_dataset notebook for hotel data model

--- notebooks/week_2/01.prepare_dataset.py
+++ notebooks/week_2/01.prepare_dataset.py
@@ -1,6 +1,6 @@
 # Databricks notebook source
-from house_price.data_processor import DataProcessor
-from house_price.config import ProjectConfig
+from hotel_reservations.data_processor import DataProcessor
+from hotel_reservations.config import ProjectConfig
 from datetime import datetime
 import pandas as pd
 from pyspark.sql import SparkSession
@@ -12,14 +12,17 @@ spark = SparkSession.builder.getOrCreate()
 config = ProjectConfig.from_yaml(config_path="../../project_config.yml")
 
 # COMMAND ----------
-# Load the house prices dataset
+# Load the hotel reservation dataset
 df = spark.read.csv(
-    "/Volumes/mlops_dev/house_prices/data/data.csv",
-    header=True,
-    inferSchema=True).toPandas()
+    "/Volumes/heiaepgah71pwedmld01001/mk_test/mlops_data/Hotel Reservations.csv", header=True, inferSchema=True
+).toPandas()
 
 # COMMAND ----------
 data_processor = DataProcessor(pandas_df=df, config=config)
-data_processor.preprocess()
-train_set, test_set = data_processor.split_data()
+data_processor.preprocess_data()
+X_train, X_test, y_train, y_test = data_processor.split_data()
+train_set = pd.concat([X_train, y_train], axis=1)
+test_set = pd.concat([X_test, y_test], axis=1)
 data_processor.save_to_catalog(train_set=train_set, test_set=test_set, spark=spark)
+
+# COMMAND ----------
